8faad7e74d8a998c5cc67598ce3a34f75f4c9192
root <root@gpu-cloud-vnode145.dakao.io>
2019-04-11 14:46:58 +0900

    script changed.

--- scripts/run_pretraining.sh
+++ scripts/run_pretraining.sh
@@ -1,9 +1,9 @@
 export PYTHONPATH=`pwd`
 
-python examples/lm_finetuning/pregenerate_training_data.py \
+python examples/lm_finetuning/finetune_on_pregenerated.py \
     --pregenerated_data=training/ \
-    --bert_config=../../samples/bert_config.json \
+    --bert_config=samples/bert_config.json \
     --output_dir=finetuned_lm/ \
     --epochs=4 \
     --train_batch_size=32 \
-    --vocab_file=../../samples/hangul_vocab.txt
+    --vocab_file=samples/hangul_vocab.txt
